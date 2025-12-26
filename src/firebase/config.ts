d2a4979462237aa80402f25da94abf81ce5e894a
CAMPKOREA GIDOKYO <campkorea.org@gmail.com>
2025-12-26 03:30:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5961429873-f934f",
+  "appId": "1:232265112606:web:f905ce931976ebbda8a20f",
+  "apiKey": "AIzaSyCFMOFgwWW4i9SYCAkR5Snmvj1Rde0BCfw",
+  "authDomain": "studio-5961429873-f934f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "232265112606"
+};
