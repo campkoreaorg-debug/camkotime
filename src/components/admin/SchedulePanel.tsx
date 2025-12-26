2964d2a9bb2c2b9c91f987fa64725223f94e42c6
CAMPKOREA GIDOKYO <campkorea.org@gmail.com>
2025-12-26 07:55:19 +0000

    Try fixing this error: `Runtime ReferenceError: Controller is not define

--- src/components/admin/SchedulePanel.tsx
+++ src/components/admin/SchedulePanel.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import { useState, useMemo } from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 import { Plus, Trash2, Edit, User } from 'lucide-react';
