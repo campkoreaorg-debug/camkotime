562a75b5af61af2f5b3b071718cac1271b41fcd6
CAMPKOREA GIDOKYO <campkorea.org@gmail.com>
2026-01-02 04:03:13 +0000

    스태프 추가할 때 생기는 Firebase Storage: No default bucket found. Did you set the

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,8 @@
+
 export const firebaseConfig = {
   "projectId": "studio-5961429873-f934f",
   "appId": "1:232265112606:web:f905ce931976ebbda8a20f",
+  "storageBucket": "studio-5961429873-f934f.appspot.com",
   "apiKey": "AIzaSyCFMOFgwWW4i9SYCAkR5Snmvj1Rde0BCfw",
   "authDomain": "studio-5961429873-f934f.firebaseapp.com",
   "measurementId": "",
